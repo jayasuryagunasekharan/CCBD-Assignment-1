027d01339d45480fed3990b1e36bd2d985146601
Jayasurya Gunasekharan <jayasurya.guna@gmail.com>
2023-06-12 04:29:23 -0500

    test1 changes

--- app.py
+++ app.py
@@ -4,15 +4,18 @@ import os
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = './static/'
+app.config['UPLOAD_FOLDER'] = './static/uploads'
+
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
     return render_template('myindexpage.html')
 
+
 @app.route("/upload", methods=['GET', 'POST'])
 def upload():
     return render_template('myuploadpage.html')
+
 
 @app.route("/data", methods=['GET', 'POST'])
 def data():
@@ -34,15 +37,17 @@ def data():
 
     return render_template('mydatapage.html')
 
+
 @app.route("/search", methods=['GET', 'POST'])
 def search():
     return render_template('mysearchpage.html')
+
 
 @app.route("/searchimage", methods=['GET', 'POST'])
 def search_image():
     if request.method == 'POST':
         name = request.form['name']
-        csv_reader = csv.DictReader(open('static/people.csv'))
+        csv_reader = csv.DictReader(open('static/uploads/people.csv'))
         temp_path = ''
         for row in csv_reader:
             if name == row['Name']:
@@ -52,35 +57,48 @@ def search_image():
         else:
             return render_template('mysearchpage.html', error="Picture not found :(")
 
+
 @app.route("/searchbysal", methods=['GET', 'POST'])
 def search_by_salary():
-    csv_reader = csv.DictReader(open('static/people.csv'))
-    temp_path = []
+    if request.method == 'POST':
+        csv_reader = csv.DictReader(open('static/uploads/people.csv'))
+        start_range = request.form['startRange']
+        end_range = request.form['endRange']
+        is_number_range = False
 
-    for row in csv_reader:
-        if row['Salary'] == '' or row['Salary'] == ' ':
-            row['Salary'] = 99000
-        if int(float(row['Salary'])) < 99000:
-            if row['Picture'] != ' ':
-                temp_path.append('static/' + row['Picture'])
-                print(temp_path)
-                print(int(float(row['Salary'])))
+        if start_range and end_range:
+            is_number_range = True
+            start_range = int(start_range)
+            end_range = int(end_range)
 
-    print(len(temp_path))
-    if temp_path != '':
-        return render_template('mysearchbysalpage.html', image_path=temp_path, message="Found Data!!")
-    else:
-        return render_template('mysearchbysalpage.html', error="Picture not found :(")
+        temp_path = []
+        data = []
+
+        for row in csv_reader:
+            if is_number_range:
+                if row['Num'].strip() != '' and start_range <= int(row['Num']) <= end_range or row['Year'].strip() != '' and start_range <= int(row['Year']) <= end_range:
+                    if row['Picture'] != ' ':
+                        temp_path.append('static/' + row['Picture'])
+                    data.append(row)
+
+        if temp_path:
+            return render_template('mysearchbysalpage.html', data=data, image_path=temp_path, message="Found Data!!")
+        else:
+            return render_template('mysearchbysalpage.html', error="No information or picture available.")
+
+    return render_template('mysearchbysalpage.html')
+
 
 @app.route("/edit", methods=['GET', 'POST'])
 def edit():
     return render_template('myeditpage.html')
 
+
 @app.route("/editdetails", methods=['GET', 'POST'])
 def edit_details():
     if request.method == 'POST':
         name = request.form['name']
-        csv_reader = csv.DictReader(open('static/people.csv'))
+        csv_reader = csv.DictReader(open('static/uploads/people.csv'))
         temp_name = ''
         for row in csv_reader:
             if name == row['Name']:
@@ -90,23 +108,26 @@ def edit_details():
         else:
             return render_template('mydisplaypage.html', error="Record not found :(")
 
+
 @app.route("/updatedetails", methods=['GET', 'POST'])
 def update_details():
     if request.method == 'POST':
         name = request.form['name']
         state = request.form['state']
         salary = request.form['salary']
-        grade = request.form['grade']
+        num = request.form['num']
         room = request.form['room']
+        year = request.form['year']
         telnum = request.form['telnum']
         picture = request.form['picture']
-        keyword = request.form['keyword']
+        comments = request.form['comments']
         cnt = 0
 
-        temp = [name, state, salary, grade, room, telnum, picture, keyword]
+        temp = [name, state, salary, num, room,
+                year, telnum, picture, comments]
         line = list()
 
-        with open('static/people.csv', 'r') as f1:
+        with open('static/uploads/people.csv', 'r') as f1:
             csv_reader = csv.reader(f1)
             for row in csv_reader:
                 if name == row[0]:
@@ -115,7 +136,7 @@ def update_details():
                     line.append(row)
                 cnt += 1
 
-            csv_write = open('static/people.csv', 'w')
+            csv_write = open('static/uploads/people.csv', 'w')
             for i in line:
                 for j in i:
                     csv_write.write(j + ',')
@@ -126,9 +147,11 @@ def update_details():
             else:
                 return render_template('mydisplaypage.html', error="Record not found :(")
 
+
 @app.route("/remove", methods=['GET', 'POST'])
 def remove():
     return render_template('myremovepage.html')
+
 
 @app.route("/removedetails", methods=['GET', 'POST'])
 def remove_details():
@@ -136,7 +159,7 @@ def remove_details():
         name = request.form['name']
         cnt = 0
         line = list()
-        with open('static/people.csv', 'r') as f1:
+        with open('static/uploads/people.csv', 'r') as f1:
             csv_reader = csv.reader(f1)
             for row in csv_reader:
                 line.append(row)
@@ -144,7 +167,7 @@ def remove_details():
                     line.remove(row)
                     cnt += 1
 
-            csv_write = open('static/people.csv', 'w')
+            csv_write = open('static/uploads/people.csv', 'w')
             for i in line:
                 for j in i:
                     csv_write.write(j + ',')
@@ -155,9 +178,11 @@ def remove_details():
         else:
             return render_template('myremovedetailspage.html', error="Record not found :(")
 
+
 @app.route("/uploadpic", methods=['GET', 'POST'])
 def upload_pic():
     return render_template('myuploadpicpage.html')
+
 
 @app.route("/uploadnew", methods=['GET', 'POST'])
 def upload_new():
@@ -165,6 +190,7 @@ def upload_new():
         file = request.files['img']
         file.save('static/' + file.filename)
         return render_template('myuploaddisplaypage.html', msg="Image Uploaded Successfully.")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
